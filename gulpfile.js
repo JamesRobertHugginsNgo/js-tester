7ffc76d274f97c06ab4e63ec21c5737f67604589
James Robert Huggins Ngo <jamesroberthugginsngo@gmail.com>
2021-12-16 03:35:39 -0500

    Add terser

--- gulpfile.js
+++ gulpfile.js
@@ -3,29 +3,44 @@ const gulp = require('gulp');
 const gulpBabel = require('gulp-babel');
 const gulpPreProcess = require('gulp-preprocess');
 const gulpSourceMaps = require('gulp-sourcemaps');
+const gulpTerser = require('gulp-terser');
 const gulpUglify = require('gulp-uglify');
 
 function cleanup() {
 	return del('dist');
 }
 
-function browserBuild() {
+function browserEs5Build() {
 	return gulp.src('src/**/*.js')
 		.pipe(gulpPreProcess({ context: { BROWSER: true } }))
 		.pipe(gulpSourceMaps.init())
 		.pipe(gulpBabel())
 		.pipe(gulpUglify())
 		.pipe(gulpSourceMaps.write('.'))
-		.pipe(gulp.dest('dist/browser'));
+		.pipe(gulp.dest('dist/browser/es5'));
+}
+
+function browserEs6Build() {
+	return gulp.src('src/**/*.js')
+		.pipe(gulpPreProcess({ context: { BROWSER: true } }))
+		.pipe(gulpSourceMaps.init())
+		.pipe(gulpTerser())
+		.pipe(gulpUglify())
+		.pipe(gulpSourceMaps.write('.'))
+		.pipe(gulp.dest('dist/browser/es6'));
 }
 
 function nodeBuild() {
 	return gulp.src('src/**/*.js')
 		.pipe(gulpPreProcess({ context: { NODE: true } }))
+		.pipe(gulpSourceMaps.init())
+		.pipe(gulpTerser())
+		.pipe(gulpUglify())
+		.pipe(gulpSourceMaps.write('.'))
 		.pipe(gulp.dest('dist/node'));
 }
 
-const build = gulp.parallel(browserBuild, nodeBuild);
+const build = gulp.parallel(nodeBuild, browserEs5Build, browserEs6Build);
 
 module.exports = {
 	build: gulp.series(cleanup, build)
