4e394d845b0c9c1de745bdda60be6cc082a9b61e
James Robert Huggins Ngo <jamesroberthugginsngo@gmail.com>
2021-12-17 10:49:47 -0500

    Update tests

--- test/node/index.js
+++ test/node/index.js
@@ -1,23 +1,23 @@
-const jsTest = require('../../dist/node/js-tester');
+const jsTester = require('../../dist/node/js-tester');
 
 Promise.resolve()
 	/////////////////////////////////////////////////////////////////////////////
 	// TEST 1
 	/////////////////////////////////////////////////////////////////////////////
-	.then(jsTest('TESTER 1', () => {
+	.then(jsTester('TESTER 1', () => {
 		return 'TEST';
-	}).test('TEST 1', (value) => {
+	}).test('TEST 1.1', (value) => {
 		return value === 'TEST';
-	}).test('TEST 2', (value) => {
+	}).test('TEST 1.2', (value) => {
 		return value !== 'TEST';
 	}).func())
 	/////////////////////////////////////////////////////////////////////////////
 	// TEST 2
 	/////////////////////////////////////////////////////////////////////////////
-	.then(jsTest('TESTER 2', () => {
+	.then(jsTester('TESTER 2', () => {
 		return 'TEST';
-	}).test('TEST 1', (value) => {
+	}).test('TEST 2.1', (value) => {
 		return value === 'TEST';
-	}).test('TEST 2', (value) => {
+	}).test('TEST 2.2', (value) => {
 		return value !== 'TEST';
 	}).func());
